e6b2ce568cb7a802aeb44ee981988b75c85dc1a3
Melih Darcan <57872471+MelihDarcanxyz@users.noreply.github.com>
2023-04-16 14:01:36 +0300

    feat: write schema for recent collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,56 @@
-// 1. Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content';
-// 2. Define a schema for each collection you'd like to validate.
-const defaultCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    author: z.enum(["John Doe", "Jane Doe"]),
-    date: z.date(),
-    tags: z.array(z.string()),
-    draft: z.boolean(),
-    image: z.string().optional(),
-  }),
+import { z, defineCollection } from "astro:content";
+
+const imagePath = z.string().optional()
+
+const pagesCollection = defineCollection({
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        image: imagePath,
+    }),
 });
+
+const projects = {
+    crossbar: "crossbar",
+    druggen: "druggen",
+    protgen: "protgen",
+};
+
+const projectSet = z
+    .array(z.nativeEnum(projects))
+    .transform((arr) => new Set(arr));
+
+const contactUrl = z.string().url().optional();
+
+const membersCollection = defineCollection({
+    schema: z.object({
+        name: z.string(),
+        surname: z.string(),
+        contacts: z.object({
+            github: contactUrl,
+            linkedin: contactUrl,
+            website: contactUrl,
+            twitter: contactUrl,
+        }),
+        current_projects: projectSet,
+        former_projects: projectSet,
+        image: imagePath,
+    }),
+});
+
+const projectCollection = defineCollection({
+    schema: z.object({
+        name: z.string(),
+        start_date: z.string(),
+        end_date: z.string(),
+        status: z.boolean(),
+        image: imagePath,
+    }),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
+    members: membersCollection,
+    pages: pagesCollection,
+    projects: projectCollection,
 };
