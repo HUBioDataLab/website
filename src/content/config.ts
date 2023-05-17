8143f8a106c5651ae0b8d4e45946ca34ded36685
Melih Darcan <57872471+MelihDarcanxyz@users.noreply.github.com>
2023-05-17 19:22:21 +0300

    feat: declare new collections chore: refactor existing code

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,22 +10,18 @@ const pagesCollection = defineCollection({
     }),
 });
 
-const projects = {
-    crossbar: "crossbar",
-    druggen: "druggen",
-    protgen: "protgen",
-};
+const projects = z.enum(["crossbar", "druggen", "protgen", "other"]);
 
 const projectSet = z
-    .array(z.nativeEnum(projects))
+    .array(projects)
     .transform((arr) => new Set(arr));
 
 const contactUrl = z.string().url().optional();
 
 const membersCollection = defineCollection({
     schema: z.object({
-        name: z.string(),
-        surname: z.string(),
+        title: z.string(),
+        description: z.string(),
         contacts: z.object({
             github: contactUrl,
             linkedin: contactUrl,
@@ -34,17 +30,30 @@ const membersCollection = defineCollection({
         }),
         current_projects: projectSet,
         former_projects: projectSet,
+        draft: z.boolean(),
         image: imagePath,
     }),
 });
 
 const projectCollection = defineCollection({
     schema: z.object({
-        name: z.string(),
+        title: z.string(),
+        description: z.string(),
         start_date: z.string(),
         end_date: z.string(),
         status: z.boolean(),
         image: imagePath,
+        draft: z.boolean()
+    }),
+});
+
+const publicationCollection = defineCollection({
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        year: z.number(),
+        project: z.string(),
+        draft: z.boolean(),
     }),
 });
 
@@ -53,4 +62,5 @@ export const collections = {
     members: membersCollection,
     pages: pagesCollection,
     projects: projectCollection,
+    publications: publicationCollection
 };
