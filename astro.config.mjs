e0a224d7b1fea554414bd96b635ccc9932994f7a
Melih Darcan <57872471+MelihDarcanxyz@users.noreply.github.com>
2023-05-17 19:22:21 +0300

    chore: format file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
@@ -10,22 +10,22 @@ import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
 
 // https://github.com/shishkin/astro-pagefind
-import pagefind from 'astro-pagefind';
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:3000/",
-  integrations: [mdx(), robotsTxt(), sitemap(), pagefind()],
-  markdown: {
-    shikiConfig: {
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'rose-pine-moon',
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      // langs: ['python'], Not needed for now
-      // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
+    site: "http://localhost:3000/",
+    integrations: [mdx(), robotsTxt(), sitemap(), pagefind()],
+    markdown: {
+        shikiConfig: {
+            // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+            theme: "rose-pine-moon",
+            // Add custom languages
+            // Note: Shiki has countless langs built-in, including .astro!
+            // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+            // langs: ['python'], Not needed for now
+            // Enable word wrap to prevent horizontal scrolling
+            wrap: true,
+        },
     },
-  },
 });
