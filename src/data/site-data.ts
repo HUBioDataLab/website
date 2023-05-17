e1e3d8e458b20996f03e1cee9c98128af4ca0f8a
Melih Darcan <57872471+MelihDarcanxyz@users.noreply.github.com>
2023-05-17 19:22:21 +0300

    chore: correct site data

--- src/data/site-data.ts
+++ src/data/site-data.ts
@@ -1,7 +1,7 @@
 // Site config
-export const SITE_TITLE: string = "xyz-astro";
+export const SITE_TITLE: string = "hu biolab";
 export const SITE_DESCRIPTION: string = "";
 
 // Author config
-export const AUTHOR: string = "John Doe";
+export const AUTHOR: string = "Melih DARCAN";
 export const EMAIL: string = "john_doe@examplemail.com";
